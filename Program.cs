596ebd0fe9a2727fc3f44801e525bc5b0acd5c5b
Elizete <Elizete@DESKTOP-LM5NDB9>
2021-11-08 19:56:12 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Livraria2
+{
+    class Cliente 
+    {
+        public string nome;
+        public string telefone;
+        public string cpf;
+        public string endereço;
+
+
+        {
+            Console.WriteLine("Nome:" + nome);
+            Console.Writeline("telefone" + telefone);
+            Console.Writeline("cpf" + cpf);
+            Console.Writeline("Endereço" + endereço);
+        }
+    }
+}
+ 
